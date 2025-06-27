01f8640825c87b92d6d9ca97deda501b882ee044
EivisMat <149148274+EivisMat@users.noreply.github.com>
2025-06-27 14:46:16 +0300

    Initial commit
    Removed all previous code for refactoring, Will re-do from scratch.

--- GameList/backend/Models/GameListDatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace GameList.Models;
-
-public class GameListDatabaseSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string GamesCollectionName { get; set; } = null!;
-}
