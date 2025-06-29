00438b7e39121785344ef818f8ea64e542b9e150
EivisMat <149148274+EivisMat@users.noreply.github.com>
2025-06-29 16:02:39 +0300

    Update: Game.cs
    Add methods:
    - `AddOwner`: void method, takes `Person` as argument. Attempts to add person to the `Owners` dictionary
    - `RemoveOwner`: void method, takes `Person` as argument. Attempts to remove person from the `Owners` dictionary
    - `SetOwner`: void method, takes `Person` and `bool` as arguments. Attempts to set person's ownership to the provided boolean

--- GameList/backend/Models/Domain/Game.cs
+++ GameList/backend/Models/Domain/Game.cs
@@ -10,4 +10,27 @@ public class Game {
     public Dictionary<Guid, bool> Owners { get; set; } = new();
 
     public bool IsExcluded { get; set; } = false;
+
+    public void AddOwner(Person person) {
+        if (Owners.Any(o => o.Key == person.Id)) {
+            throw new InvalidOperationException("Person already exists.");
+        }
+
+        Owners.Add(person.Id, false);
+    }
+
+    public void RemoveOwner(Person person) {
+        if (!Owners.Remove(person.Id)) {
+            throw new InvalidOperationException("Person doesn't exist.");
+        }
+    }
+
+    public void SetOwner(Person person, bool IsOwner) {
+        if (Owners.ContainsKey(person.Id)) {
+            Owners[person.Id] = IsOwner;
+        }
+        else {
+            throw new InvalidOperationException("Person doesn't exist.");
+        }
+    }
 }
