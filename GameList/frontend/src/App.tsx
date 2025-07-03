1d55eaa3a0ecd23dc303d091d25d0be0809fa1df
EivisMat <149148274+EivisMat@users.noreply.github.com>
2025-07-03 17:03:34 +0300

    Add: structure
    ### Changes
    
    - Created skeleton structure for frontend (will likely be expanded in the future)
    - Set up basename so it all runs under `/thelist/`
    
    ### To-do
    
    - Create home page
    - Create list page
    - API calls
    - Basically everything else

--- GameList/frontend/src/App.tsx
+++ GameList/frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import List from './pages/List';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
